33e9c36c0de5f44df64fbfa0bcb824bfefdb1ac8
swyoo5 <rongleader@naver.com>
2024-10-17 16:00:31 +0900

    jpa 시작

---
+++ src/main/java/org/pgm/jpademo/controller/SampleController.java
@@ -0,0 +1,71 @@
+package org.pgm.jpademo.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@Controller
+@Log4j2
+public class SampleController {
+    @GetMapping("/hello")
+    public void hello(Model model) {
+        log.info("hello");
+        model.addAttribute("msg", "hello world");
+    }
+
+    @GetMapping("/hello1")
+    public void hello1(@RequestParam("name") String name,
+                       @RequestParam("age") int age,
+                       Model model) {
+        log.info("hello1");
+        log.info("name: " + name);
+        log.info("age: " + age);
+        model.addAttribute("name", name);
+        model.addAttribute("msg", "hello world");
+    }
+
+    @GetMapping("/hello2")
+    public void hello2(Model model) {
+        log.info("hello2");
+        model.addAttribute("msg", "hello world");
+    }
+    @GetMapping("/hello3")
+    public void hello3(Model model) {
+        log.info("hello3");
+        model.addAttribute("msg", "hello world");
+    }
+
+    @GetMapping("/ex/ex1")
+    public void ex1(Model model) {
+        log.info("ex1");
+        List<String> list= Arrays.asList("AAAA", "BBBB", "CCCC");
+        model.addAttribute("list", list);
+    }
+
+    @GetMapping("/ex/ex2")
+    public void ex2(Model model) {
+        log.info("ex2");
+
+        List<String> strList2=new ArrayList<>();
+        List<String> strList= IntStream.range(1, 10)
+                .mapToObj(i->"Data"+i)
+                .collect(Collectors.toList());
+//        같은 역할
+        for (int i = 1; i < 10; i++) {
+            strList2.add("Data"+i);
+        }
+        Map<String, Integer> maps=new HashMap<> ();
+        maps.put("홍길동", 80);
+        maps.put("박경미", 75);
+        maps.put("윤요섭", 85);
+        model.addAttribute("maps", maps);
+        model.addAttribute("strList", strList);
+        model.addAttribute("strList2", strList2);
+    }
+}
